c2f4a60187ed796e0a0500e36f90ed2e80b15d7b
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2025-05-30 00:59:47 +0200

    Agregado botón y temas con tres diferentes paletas de colores

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const flashcard = document.getElementById("flashcard");
 const cardInner = flashcard.querySelector(".card-inner");
 const questionEl = document.getElementById("question");
 const answerEl = document.getElementById("answer");
+const selector = document.getElementById("themeSelector");
 
 async function loadFlashcards() {
     const res = await fetch("flashcards.json");
@@ -31,5 +32,12 @@ function prevCard() {
 flashcard.addEventListener("click", () => {
     flashcard.classList.toggle("flipped");
 });
+
+selector.addEventListener("change", (e) => {
+    document.body.classList.remove("theme-1", "theme-2", "theme-3");
+    document.body.classList.add(e.target.value);
+});
+
+document.body.classList.add("theme-4");
 
 loadFlashcards();
