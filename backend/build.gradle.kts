d12ca241f674cb56e0887e84737de3ea6448755e
Cristian Sindila <128726794+CristianInBits@users.noreply.github.com>
2025-09-24 17:32:15 +0200

    fix: fixed compatibility JAVA JDK

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -10,7 +10,7 @@ description = "Flashcards Web – Backend (MVP)"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,1 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    // Permite a Gradle descargar el JDK 21 automáticamente
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "flashcards-app-backend"
