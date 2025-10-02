85549b2c62089d87f9fc655273a51f62a44e541f
Cristian Sindila <128726794+CristianInBits@users.noreply.github.com>
2025-10-02 16:02:31 +0200

    usuarios service

---
+++ backend/src/main/java/com/csindila/flashcards/auth/service/AppUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.csindila.flashcards.auth.service;
+
+import com.csindila.flashcards.auth.repo.AppUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository users;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var u = users.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+        return new User(u.getEmail(), u.getPasswordHash(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + u.getRole())));
+    }
+}
